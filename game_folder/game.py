7f3c03f521156984a66fa548c6dcef5a95e5a1ca
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-01-21 21:07:01 +0000

    Delete game_folder directory

--- game_folder/game.py
+++
@@ -1,7 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-class Game(Screen):
-    def back_menu(self):
-        self.parent.transition.direction = 'left'
-        self.parent.current = 'main_menu_screen' 
-    pass
