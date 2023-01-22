41120de0403de63e0a9ebd7c557cac3517ac0af4
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-01-22 02:31:09 +0000

    Add files via upload
    remember of change values of win and lose

---
+++ Jokenpo_4.0/mecanic1vComputer.py
@@ -0,0 +1,29 @@
+import pygame
+import random
+
+class Mecanic1vComputer():
+    def __init__(self, player):
+        computer = random.randint(1, 3)
+        
+        self.computer_result = computer
+
+        #Tie
+        if player == computer:
+            self.result = 0
+        
+        else:
+            #Player Won
+            if player == 1 and computer == 3:
+                self.result = 1
+            elif player == 2 and computer == 1:
+                self.result = 1
+            elif player == 3 and computer == 2:
+                self.result = 1
+
+            #Player Lose
+            elif player == 3 and computer == 1:
+                self.result = 2
+            elif player == 1 and computer == 2:
+                self.result = 2
+            elif player == 2 and computer == 3:
+                self.result = 2
